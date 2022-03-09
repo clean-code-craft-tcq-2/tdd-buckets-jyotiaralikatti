8a038a5a1af8d2e5ae1fdcf3b37d944da8be686c
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-03-09 08:30:50 +0530

    Add files via upload

---
+++ Test_GetChargingCurrentValues.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN 
+
+#include "test/catch.hpp"
+#include "GetChargingCurrentValues.h"
+
+TEST_CASE("Checks the charging current ranges and get the no of occurences") {
+  int CurrentValues[] = {4,5};
+  REQUIRE(GetChargingCurrentValues(CurrentValues,2) == 1);
+}
