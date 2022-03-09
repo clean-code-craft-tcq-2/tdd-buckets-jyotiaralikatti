0f2f6a014d2be5c888a8220b0d98da69388ca28e
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-03-09 09:21:52 +0530

    Update GetChargingCurrentValues.c

--- GetChargingCurrentValues.c
+++ GetChargingCurrentValues.c
@@ -3,23 +3,30 @@
 
 int GetChargingCurrentValues (int *CurrentValues, int TotalCurrentValues){
   int TotalPeriodicSetRanges = 0;
-  int i,temp;
-  
-  /* to sort values in an order */
-  for(i = 0; i < TotalCurrentValues ; i++)
-  {  
-	if(CurrentValues[i] > CurrentValues[i+1])
-    {
-      temp = CurrentValues[i];
-      CurrentValues[i] = CurrentValues[i+1];
-      CurrentValues[i+1] = temp;
-      i= -1;	  
-	}
-   }
+  int i;
+  /* Call to sort values in an order */
+  SortCurrentValues(CurrentValues,TotalCurrentValues);
+
   /* arrage and the ranges */
 	if (  (TotalCurrentValues == 1) || (TotalCurrentValues == 2))
 	{ 
 		TotalPeriodicSetRanges = TotalCurrentValues;
 	}
   return TotalPeriodicSetRanges;
+}
+void SortCurrentValues(int *CurrentValues, int TotalCurrentValues)
+{
+	 int i,temp;
+/* to sort values in an order */
+for(i = 0; i < TotalCurrentValues ; i++)
+{  
+	if(CurrentValues[i] > CurrentValues[i+1])
+		{
+		temp = CurrentValues[i];
+		CurrentValues[i] = CurrentValues[i+1];
+		CurrentValues[i+1] = temp;
+		i= -1;	  
+		}
+}
+
 }
