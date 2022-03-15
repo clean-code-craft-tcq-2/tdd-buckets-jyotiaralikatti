d840874751f936d71bd463d9ade8df2d5509db06
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-03-15 19:16:33 +0530

    Update GetChargingCurrentValues.h

--- GetChargingCurrentValues.h
+++ GetChargingCurrentValues.h
@@ -1,1 +1,11 @@
+typedef struct 
+{
+    int lowerRange;
+    int upperRange;
+	int NumberOfPeriodicRanges;
+}Range_Cfg;
+
+Range_Cfg GetPeriodicSetRanges(int *CurrentValues, int TotalCurrentValues);
+void SortCurrentValues(int *CurrentValues, int TotalCurrentValues);
 int GetChargingCurrentValues (int *CurrentValues, int TotalCurrentValues);
+
