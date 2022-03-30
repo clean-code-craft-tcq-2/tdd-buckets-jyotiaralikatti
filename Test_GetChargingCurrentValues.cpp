669588fd85576d264b990b98adf63ea3397dbfa1
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-03-30 11:10:55 +0530

    Update Test_GetChargingCurrentValues.cpp

--- Test_GetChargingCurrentValues.cpp
+++ Test_GetChargingCurrentValues.cpp
@@ -16,14 +16,14 @@ TEST_CASE("Case1 : For inputs in sorted order") {
   REQUIRE(strcmp(actual_result, expected_range) == 0);
 }
 
-TEST_CASE("Case1 : For inputs in Unsorted order") {
-     int input_range[]= {3, 5, 4, 8, 10, 9, 7};
-  const char *expected_range = "3-5,3\n7-10,4";
-  char readingRange[MAX_STRING_LEN]={0};
+TEST_CASE("Case2 : For inputs in Unsorted order") {
+     int input_range2[]= {3, 5, 4, 8, 10, 9, 7};
+  const char *expected_range2 = "3-5,3\n7-10,4";
+  char readingRange2[MAX_STRING_LEN]={0};
   
-  int no_of_elements = sizeof(input_range)/sizeof(input_range[0]);
-  char *actual_result;
+  int no_of_elements = sizeof(input_range2)/sizeof(input_range2[0]);
+  char *actual_result2;
 
-  actual_result = GetPeriodicSetRanges(input_range, no_of_elements, Index, readingRange);
-  REQUIRE(strcmp(actual_result, expected_range) == 0);
+  actual_result = GetPeriodicSetRanges(input_range2, no_of_elements, Index, readingRange2);
+  REQUIRE(strcmp(actual_result, expected_range2) == 0);
 }
