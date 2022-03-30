4e01331045e077f11399db2d1260496d384bb495
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-03-30 10:57:04 +0530

    Update Test_GetChargingCurrentValues.cpp

--- Test_GetChargingCurrentValues.cpp
+++ Test_GetChargingCurrentValues.cpp
@@ -3,21 +3,14 @@
 #include "test/catch.hpp"
 #include "GetChargingCurrentValues.h"
 
-TEST_CASE("Checks the charging current ranges and get the no of occurences") {
-  int CurrentValues[] = {4,5};
-  REQUIRE(GetChargingCurrentValues(CurrentValues,2) == 1);
-}
-TEST_CASE("Checks the charging current if there are same ranges and get the number of occurences") {
-  int CurrentValues[] = {4,4};
-  REQUIRE(GetChargingCurrentValues(CurrentValues,2) == 1);
-}
+TEST_CASE("detect multiple ranges and check if the number of readings of each range matches the expected value.") {
+     int input_range[]={3,4,5,8,9};// {3, 5, 4, 8, 10, 9, 7};
+  const char *expected_range = "3-5,3\n8-9,2";//3-5,3\n7-10,4";
+  char readingRange[MAX_STRING_LEN]={0};
+  
+  int no_of_elements = sizeof(input_range)/sizeof(input_range[0]);
+  char *actual_result;
 
-TEST_CASE("Checks the periodicity of charging current ranges and get the no of occurences for sequectial inputs") {
-  int CurrentValues[] = {4,5,6};
-  REQUIRE(GetChargingCurrentValues(CurrentValues,3) == 1);
-}
-
-TEST_CASE("Checks the periodicity of charging current ranges and get the no of occurences for non sequential inputs") {
-  int CurrentValues[] = {9,5,4,8,5};
-  REQUIRE(GetChargingCurrentValues(CurrentValues,5) == 2);
+  actual_result = GetPeriodicSetRanges(input_range, no_of_elements, Index, readingRange);
+  REQUIRE(strcmp(actual_result, expected_range) == 0);
 }
