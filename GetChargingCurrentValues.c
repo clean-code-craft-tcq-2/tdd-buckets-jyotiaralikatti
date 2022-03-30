17ba0d5b47d18a9606a077ae73c34681df3655df
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-03-30 10:56:59 +0530

    Update GetChargingCurrentValues.c

--- GetChargingCurrentValues.c
+++ GetChargingCurrentValues.c
@@ -1,49 +1,28 @@
 #include <stdio.h>
-#include<assert.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 #include "GetChargingCurrentValues.h"
-Range_Cfg range;
-int TotalPeriodicSetRanges;
-int GetChargingCurrentValues (int *CurrentValues, int TotalCurrentValues){
-  int i;
-  SortCurrentValues(CurrentValues,TotalCurrentValues);
 
-   range = GetPeriodicSetRanges(CurrentValues,TotalCurrentValues);
-  return range.NumberOfPeriodicRanges;
-}
-void SortCurrentValues(int *CurrentValues, int TotalCurrentValues)
-{
-  int i,swap;
-  for(i = 0; i <(TotalCurrentValues - 1); i++){
-    if(CurrentValues[i] > CurrentValues[i+1]){
-      swap = CurrentValues[i];
-      CurrentValues[i] = CurrentValues[i+1];
-      CurrentValues[i+1] = swap;
-      i = -1;
-    }
-  }  
-}
-int GetNumberOfPeriodicRanges( int RangeDifference)
-{
+char* GetPeriodicSetRanges (int *CurrentValues, int TotalCurrentValues, int Index, char *result);
+char output_string[20] = "\0";
+int Index = 0;
 
-    if( (RangeDifference == 1)){
-        TotalPeriodicSetRanges++;
-    }
-  
-    return TotalPeriodicSetRanges;
-}
-Range_Cfg GetPeriodicSetRanges(int *CurrentValues, int TotalCurrentValues)
+char* GetPeriodicSetRanges (int *CurrentValues, int TotalCurrentValues, int Index, char *result)
 {
-  int i;
-    for (i=1;i<TotalCurrentValues;i++){
-    int CurrentElement,NextElement,RangeDifference;
-
-    CurrentElement = CurrentValues[i-1];
-    NextElement = CurrentValues[i];
-    RangeDifference = NextElement - CurrentElement; 
-    range. NumberOfPeriodicRanges = GetNumberOfPeriodicRanges(RangeDifference);
-    
-    if((RangeDifference == 0)&&(TotalCurrentValues == 2))
-        range. NumberOfPeriodicRanges = 1; 
-    }
-  return range;
+	int i;
+	
+	for (i = 0; i < (TotalCurrentValues - 1); i++)
+	{
+		if ((CurrentValues[i+1] - CurrentValues[i]) > 1)
+		{
+			sprintf (output_string, "%d-%d,%d\n", CurrentValues[Index],CurrentValues[i], i+1-Index);
+			Index = i+1;
+			strcat(result, output_string);
+			memset(output_string, '\0', sizeof(output_string));
+		}
+	}
+	sprintf (output_string, "%d-%d,%d", CurrentValues[Index],CurrentValues[i], i+1-Index);
+	strcat(result, output_string);
+	return result;
 }
