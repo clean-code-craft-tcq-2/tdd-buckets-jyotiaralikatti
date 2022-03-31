657881f2762f277efc722501df9ad312acafd120
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-03-31 16:01:24 +0530

    Update GetChargingCurrentValues.h

--- GetChargingCurrentValues.h
+++ GetChargingCurrentValues.h
@@ -1,9 +1,11 @@
 
+#define MAX_12BIT_CURRENTVALUE 4094
+#define MAX_CURRENT_IN_AMPS    10
+#define MAX_STRING_LEN         32
 
 extern int Index;
 extern char output_string[20];
 
 char* GetPeriodicSetRanges (int *, int , int , char *);
-
-#define MAX_STRING_LEN 32
 int cmpfunc (const void * a, const void * b);
+void ConvertAnalogCurrentInputs2Digital(int *, int , int *);
