7bcc7406da06c662c75a890d8729f5a89bd9458d
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-03-31 16:01:20 +0530

    Create AnalogCurrentInputs2Digital.c

---
+++ AnalogCurrentInputs2Digital.c
@@ -0,0 +1,21 @@
+#include "GetPeriodicSetOfRanges.h"
+#include <stdio.h>
+#include <math.h>
+
+char output_string[20] = "\0";
+int Index = 0;
+
+void ConvertAnalogCurrentInputs2Digital(int *AnalogCurrentInputs, int TotalCurrentValues, int *DigitalCurrentValues)
+{
+    int invalid=0;
+    for(int i=0; i<TotalCurrentValues;i++)
+    {
+         if (AnalogCurrentInputs[i] > MAX_12BIT_CURRENTVALUE)
+         {
+             AnalogCurrentInputs[i] = 0 ; //assume the invalid sample as 0
+             invalid++;
+         }    
+        DigitalCurrentValues[i] = (MAX_CURRENT_IN_AMPS * AnalogCurrentInputs[i] )/ MAX_12BIT_CURRENTVALUE;
+        DigitalCurrentValues[i] = round(DigitalCurrentValues[i]);
+    }
+} 
